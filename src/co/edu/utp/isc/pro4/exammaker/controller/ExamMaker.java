00b4c66e10668f69ebd51cae8dd814b1e77cc722
Esteban <estebangsan13@gmail.com>
2019-06-19 16:01:00 -0500

    Commit inicial

---
+++ src/co/edu/utp/isc/pro4/exammaker/controller/ExamMaker.java
@@ -0,0 +1,16 @@
+package co.edu.utp.isc.pro4.exammaker.controller;
+
+/**
+ *
+ * @author Esteban
+ */
+public class ExamMaker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
